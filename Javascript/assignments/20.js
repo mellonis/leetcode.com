8bf8b2572cca19a62a736867e04a40d46681d583
mellonis <mellonis@yandex.ru>
2020-07-25 22:03:03 +0500

    20th assignment

---
+++ Javascript/assignments/20.js
@@ -0,0 +1,24 @@
+const parenthesesPair = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+};
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isValid(s) {
+  const stack = [];
+
+  return [...s].every((char) => {
+    if (Object.prototype.hasOwnProperty.call(parenthesesPair, char)) {
+      stack.push(parenthesesPair[char]);
+      return true;
+    }
+
+    return stack.pop() === char;
+  }) && stack.length === 0;
+}
+
+module.exports = isValid;
---
+++ Javascript/tests/20.test.js
@@ -0,0 +1,10 @@
+const isValid = require('../assignments/20');
+
+test('from site', () => {
+  expect(isValid('()')).toBe(true);
+  expect(isValid('()[]{}')).toBe(true);
+  expect(isValid('(]')).toBe(false);
+  expect(isValid('([)]')).toBe(false);
+  expect(isValid('{[]}')).toBe(true);
+  expect(isValid('[')).toBe(false);
+});
